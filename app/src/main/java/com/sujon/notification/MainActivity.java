8b612a752335897d6027b795e81fbfbf80ce7421
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-06 03:00:42 +0600

    2_Create a Notification - Android Development with Java - Rakibul Islam - Bohubrihi
    2_Create a Notification - Android Development with Java - Rakibul Islam - Bohubrihi

--- app/src/main/java/com/sujon/notification/MainActivity.java
+++ app/src/main/java/com/sujon/notification/MainActivity.java
@@ -1,16 +1,52 @@
 package com.sujon.notification;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends AppCompatActivity {
+    String CHANNEL_ID = "MyChannel";
+    String notificationName = "MyNotification";
+    NotificationCompat.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
+        builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("My Notification Title")
+                .setContentText("This is a notification from android")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
     }
 
-    public void OnNotificationButtonClick(){}
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = notificationName;
+            String description = "For showing notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void OnNotificationButtonClick(View view){
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        
+        notificationManager.notify(1547, builder.build());
+        }
+
+
+
 
 }
